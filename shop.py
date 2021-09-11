7984f703d0c4c09a2c18783a80bb29f1060e50e1
Iliya-Yadegari <naruto3@email.com>
2021-09-11 19:55:09 +0100

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,1 +1,14 @@
-
+class Shop:
+    def __init__(self):
+        self.members = []
+    
+    def hasMember(self,memid):
+        for i in self.members:
+            if i == memid:
+                return True
+            elif i == None:
+                return False
+            else:
+                return False
+    def addMember(self,mem):
+        self.members.append(mem.id)
