7150ab41bf387c7eebbbfdcb0fc54af5c09e715d
Iliya-Yadegari <naruto3@email.com>
2021-09-15 21:02:01 +0100

    Update ShopTester.py

--- ShopTester.py
+++ ShopTester.py
@@ -17,19 +17,19 @@ print(o1.quantity)
 
 ################################################################################
 
-# print("(1.2)-------------------------------")
-# o2 = od.Order() # Be creative here since __init__ expects 3 argument, what can we do?
-# print(o2.product)
-# print(o2.price)
-# print(o2.quantity)
-# o2.product = 'Americano' 
-# o2.price = 4.7
-# o2.quantity = 2
-# print(o1.product)
-# print(o1.price)
-# print(o1.quantity)
+print("(1.2)-------------------------------")
+o2 = od.Order() # Be creative here since __init__ expects 3 argument, what can we do?
+print(o2.product)
+print(o2.price)
+print(o2.quantity)
+o2.product = 'Americano' 
+o2.price = 4.7
+o2.quantity = 2
+print(o1.product)
+print(o1.price)
+print(o1.quantity)
 
-################################################################################
+# ################################################################################
 
 print("(2)-------------------------------")
 alan = mem.Member(20.0) # a member with initial balance 20.0
@@ -39,7 +39,7 @@ alanOrders = alan.orders
 print("Alan's number of orders: " , len(alanOrders))
 print("Alan needs to pay: " , alan.amountToPay)
 
-# ##############################################################################	
+# # ##############################################################################	
 
 print("(3)-------------------------------");
 mark = mem.Member(0) # a member with initial balance 0.0
@@ -52,7 +52,7 @@ print("Mark needs to pay: " , markPay)
 mark.deposit(40.0)
 print(mark.balance)
 
-# ##############################################################################	
+# # ##############################################################################	
 
 print("(4)-------------------------------")
 alan.addOrder(o1)
@@ -64,8 +64,8 @@ print("Price of Alan's 1st order: " , alanOrder1.price);
 print("Quantity of Alan's 1st order: " , alanOrder1.quantity)
 print("Alan needs to pay: " , alan.amountToPay)
 
-# ##############################################################################	
-# 		
+# # ##############################################################################	
+# # 		
 print("(5)-------------------------------")
 alan.orderToAdd("Caffe Latte", 5.5, 1)
 alanOrders = alan.orders
@@ -80,7 +80,7 @@ print("Price of Alan's 2nd order: " , alanOrder2.price)
 print("Quantity of Alan's 2nd order: " , alanOrder2.quantity)
 print("Alan needs to pay: " , alan.amountToPay)
 
-# ##############################################################################	
+# # ##############################################################################	
 
 print("(6)-------------------------------")
 hollys = sh.Shop()
@@ -89,27 +89,27 @@ print("Number of members: " , len(members))
 print("Alan is a memebr: " , hollys.hasMember(alan.id))
 print("Mark is a memebr: " , hollys.hasMember(mark.id))
 
-# ##############################################################################	
+# # ##############################################################################	
 
 print("(7)-------------------------------")
 hollys.addMember(alan)
 hollys.addMember(mark)
 members = hollys.members
 print("Number of members: " , len(members))
-# print("Member 1's id: " , members[0].id)
-# print("Member 2's id: " , members[1].id)
+print("Member 1's id: " , members[0].id)
+print("Member 2's id: " , members[1].id)
 print("Alan is a memebr: " , hollys.hasMember(alan.id))
 print("Mark is a memebr: " , hollys.hasMember(mark.id))
-# 	
-# ##############################################################################	
-# 	
+# # 	
+# # ##############################################################################	
+# # 	
 print("(8)-------------------------------")
 # # charge an existing member: deduct their balance. 
-# hollys.checkOut(alan.id)
-# hollys.checkOut(mark.id)
-print("Member with id \"3\" exists: " , hollys.hasMember("3"))
+hollys.checkOut(alan.id)
+hollys.checkOut(mark.id)
+print("Member with id \"mem3\" exists: " , hollys.hasMember("mem3"))
 # charge a non-existing member: do nothing.
-# hollys.checkOut("3")
+hollys.checkOut("mem3")
 print("Alan's new balance: " , alan.balance)
 print("Mark's new balance: " , mark.balance)
 
