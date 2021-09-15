4894c2468e1103a81fabd530eb09030b409102c5
Iliya-Yadegari <naruto3@email.com>
2021-09-15 21:01:53 +0100

    Update member.py

--- member.py
+++ member.py
@@ -4,26 +4,31 @@ import order as od
 
 class Member():
     
-    
+    i = 0
     
     def __init__(self,balance):
         
-        self.i = 0
-        self.i += 1
+        Member.i += 1
+        self.i = 'mem' + str(Member.i)
         self.id = self.i
         self.balance = balance
         self.orders = []
-    def amountToPay(self):
-        pay = od.o1.price * od.o1.quantity
-        print(pay)
+        self.amountToPay = 0
+    # def amountToPay(self):
+    #     pay = od.o1.price * od.o1.quantity
+    #     print(pay)
         
     def deposit(self,num):
         self.balance = self.balance + num
     
     def addOrder(self,order):
         self.orders.append(order)
+        pay = order.price * order.quantity
+        self.amountToPay += pay
     
     def orderToAdd(self,productM,priceM,quantityM):
         madeOrder = od.Order(productM,priceM,quantityM)
-        
         self.orders.append(madeOrder)
+        pay = madeOrder.price * madeOrder.quantity
+        self.amountToPay += pay
+        
